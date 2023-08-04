756ecbf8bd92394b19af35c4267e34e18477eaf3
ssadichhya <sadichhya@fusemachines.com>
2023-08-04 22:29:45 +0545

    feat: formatted code using pylint and flake8

---
+++ python-intermediate/file_1.py
@@ -0,0 +1,31 @@
+import csv
+
+
+def filter_adults(input_file, output_file):
+    """_summary_
+
+    Args:
+        input_file (_type_): _description_
+        output_file (_type_): _description_
+    """
+    with open(input_file, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = [row for row in reader if int(row['Age']) >= 18]
+
+    with open(output_file, 'w', newline='') as csv_out_file:
+        fieldnames = ['Name', 'Age']
+        writer = csv.DictWriter(csv_out_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+input_file = "data.csv"
+output_file = "adults.csv"
+
+try:
+    filter_adults(input_file, output_file)
+    print(f"Filtered data saved to {output_file}.")
+except FileNotFoundError:
+    print(f"Error: {input_file} not found.")
+except Exception as e:
+    print("An unexpected error occurred:", e)
